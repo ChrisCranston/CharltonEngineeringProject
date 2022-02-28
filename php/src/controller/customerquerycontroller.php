f5a2554f44b06abecdeea8171fe49590b92e8380
Kess-strongman <kessathome@gmail.com>
2022-02-28 14:42:03 +0000

    added my sql queries and gateway

---
+++ php/src/controller/customerquerycontroller.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * APIStoredController
+ * 
+ * This class extends the controller to provide specific functionality for the stored API.
+ * 
+ * @author Chris Cranston + Kess Strongman
+ */
+class  ApiCustomerQueryController extends Controller
+{
+    /**
+     * setGateway
+     *
+     * Sets this gateway to the StoredGateway
+     */
+    protected function setGateway()
+    {
+        $this->gateway = new CustomerQueryGateway();
+    }
+
+    /**
+     * processRequest
+     *
+     * Processes the request, and calls the appropriate gateway method to perform the appropriate actions.
+     * 
+     * @return object returns the result of the gateway.
+     */
+    protected function processRequest()
+    {
+        $datetime = $this->getRequest()->getParameter("datetime");
+        $name = $this->getRequest()->getParameter("name");
+        $businessindividual = $this->getRequest()->getParameter("businessindividual");
+        $email = $this->getRequest()->getParameter("email"); //parameters = the table column fillers
+        $phonenumber = $this->getRequest()->getParameter("phonenumber");
+        $query = $this->getRequest()->getParameter("query");
+        $querytype = $this->getRequest()->getParameter("querytype");
+
+        if ($this->getRequest()->getRequestMethod() == "POST") {
+            if (!is_null($email)) { //if they haven't submitted an email 
+                $this->getGateway()->addQueryWOEmail($datetime, $name, $businessindividual, $phonenumber, $query, $querytype); 
+            } else if (!is_null($phonenumber)) { //if they haven't submitted phone
+                    $this->getGateway()->addQueryWOPhonenumber($datetime, $name, $businessindividual, $email, $query, $querytype); 
+            } else {
+                $this->getGateway()->addQueryWAll($datetime, $name, $businessindividual, $email, $phonenumber, $query, $querytype);
+            }
+        } else if ($this->getRequest()->getRequestMethod() == "GET") {
+                $this->getGateway()->GetQueryTypes();
+        } else {
+            $this->getResponse()->setMessage("Invalid Request Type.");
+            $this->getResponse()->setStatusCode(405);
+        }
+        return $this->getGateway()->getResult();
+    }
+}
---
+++ php/src/gateway/customerquerygateway.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * StoredGateway
+ * 
+ *  Stored gateway extends Gateway and formats and performs SQL queries.
+ * 
+ * @author Chris Cranston + Kess Strongman
+ */
+class CustomerQueryGateway extends Gateway
+{
+    private $pcqsql = "INSERT INTO  prospective_client_query (date_time, name, business_individual, email, phone_number, query, query_type_ id)";
+  
+    /**
+     * __construct
+     *
+     * Constructor of the gateway, set's the database value
+     */
+    public function __construct()
+    {
+        $this->setDatabase(CUSTOMER_DATABASE);
+    }
+
+    /**
+     * GetQueryTypes
+     *
+     * Finds all stored items on the stored table, ordered by surname and sets the result in the class variable. 
+     */
+    public function findAll()
+    {
+        $this->sql .= "SELECT query_type_id, query_type_desc FROM query_type";
+        $result = $this->getDatabase()->executeSQL($this->sql);
+        $this->setResult($result);
+    }
+
+    /**
+     * addQueryWAll
+     *
+     * This will add the query with if all the fields have been filled
+     * @param  int $id -  
+     */
+    public function addQueryWAll($datetime, $name, $businessoption, $email, $phonenumber, $query, $querytypeid)
+    {
+        $this->pcqsql .= " VALUES (:date_time, :name, :business_individual, :email, :phone_number, :query, :query_type_id)";
+        $params = ["date_time" => $datetime, "name" => $name, "business_individual" => $businessoption, "email" => $email, "phone_number" => $phonenumber, "query" => $query, "query_type_id" => $querytypeid];
+        $result = $this->getDatabase()->executeSQL($this->sql, $params);
+        $this->setResult($result);
+    }
+
+     /**
+     * addQueryWOEmail
+     *
+     * This will add the query with if all the fields except Email have been submitted
+     * @param  int $id -  
+     */
+    public function addQueryWOEmail($datetime, $name, $businessoption, $phonenumber, $query, $querytypeid)
+    {
+        $this->pcqsql .= " VALUES (:date_time, :name, :business_individual, :phone_number, :query, :query_type_id)";
+        $params = ["date_time" => $datetime, "name" => $name, "business_individual" => $businessoption, "phone_number" => $phonenumber, "query" => $query, "query_type_id" => $querytypeid];
+        $result = $this->getDatabase()->executeSQL($this->sql, $params);
+        $this->setResult($result);
+    }
+
+     /**
+     * addQueryWOphonenumber
+     *
+     * This will add the query with if all the fields except phonenumber have been filled
+     * @param  int $id -  
+     */
+    public function addQueryWOPhonenumber($datetime, $name, $businessoption, $email, $query, $querytypeid)
+    {
+        $this->pcqsql .= " VALUES (:date_time, :name, :business_individual, :email, :query, :query_type_id)";
+        $params = ["date_time" => $datetime, "name" => $name, "business_individual" => $businessoption, "email" => $email, "query" => $query, "query_type_id" => $querytypeid];
+        $result = $this->getDatabase()->executeSQL($this->sql, $params);
+        $this->setResult($result);
+    }
+}
